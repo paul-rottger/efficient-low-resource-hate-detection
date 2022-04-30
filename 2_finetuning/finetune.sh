214c819d443a5dded73262481d6927ff632c5278
paul-rottger <paul.roettger@yahoo.de>
2022-04-30 12:36:25 +0200

    updated bash script for arc

--- 2_finetuning/finetune.sh
+++ 2_finetuning/finetune.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-#SBATCH --partition=htc
-#SBATCH --time=24:00:00
 #SBATCH --job-name=test-finetune
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=00:10:00
+#SBATCH --partition=short 
 #SBATCH --output=test-finetune.out
 #SBATCH --error=test-finetune.err
-#SBATCH --gres=gpu:v100:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
 
 # reset modules
 module purge
 
 # load python module
-module load python/anaconda3/2019.03
+module load Anaconda3/2020.11
 
 # activate the right conda environment
-source activate $DATA/conda-envs/gab-language-change
+source activate $DATA/conda-envs/low-resource-hate
 
 # Useful job diagnostics
 #
