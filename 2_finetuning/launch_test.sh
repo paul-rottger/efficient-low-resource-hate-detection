1eab953773d5f03aedc4310faba37141441b72a4
paul-rottger <paul.roettger@yahoo.de>
2022-05-11 11:39:22 +0200

    test all ft2 models

--- 2_finetuning/launch_test.sh
+++ 2_finetuning/launch_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=test-english
+#SBATCH --job-name=test-es
 #SBATCH --clusters=arc
 #SBATCH --ntasks-per-node=16
 #SBATCH --time=10:00:00
@@ -18,7 +18,7 @@ module load Anaconda3/2020.11
 source activate $DATA/conda-envs/lrh-env
 
 for dataset in bas19_es for19_pt ous19_fr ous19_ar san20_it; do
-    for modelpath in $DATA/low-resource-hate/english-base-models/xlmt_dyn21_en_20000_rs1/; do
+    for modelpath in $DATA/low-resource-hate/finetuned-models/*${dataset}*/; do
         for testpath in $DATA/low-resource-hate/0_data/main/1_clean/${dataset}/test_*.csv; do
             python finetune_and_test.py \
                 --model_name_or_path ${modelpath} \
