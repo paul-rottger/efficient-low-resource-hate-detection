030d59392430b595293a0f2932cbca88ce02b60a
paul-rottger <paul.roettger@yahoo.de>
2022-05-18 12:29:53 +0200

    hindi finetuning

--- 2_finetuning/launch_ft2.sh
+++ 2_finetuning/launch_ft2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=ft2-xlmr-es
+#SBATCH --job-name=ft2-xlmr-hi
 #SBATCH --clusters=arc
 #SBATCH --ntasks-per-node=16
 #SBATCH --time=11:59:00
@@ -22,7 +22,7 @@ source activate $DATA/conda-envs/lrh-env
 # Pick base model to then continue finetuning (-->FT2)
 basemodel="xlmr_dyn21_en_20000_rs1"
 
-for dataset in bas19_es for19_pt ous19_ar san20_it; do
+for dataset in bas19_es for19_pt ous19_ar san20_it has19_hi has20_hi has21_hi; do
     for split in 10 20 30 40 50 100 200 300 400 500 1000 2000; do
         for seed in rs1 rs2 rs3 rs4 rs5 rs6 rs7 rs8 rs9 rs10; do
             python finetune_and_test.py \
