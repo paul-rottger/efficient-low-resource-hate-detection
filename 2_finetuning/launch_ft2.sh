8c4cae478b05d531b7c4cb38550cb505b8f64222
paul-rottger <paul.roettger@yahoo.de>
2022-06-09 15:41:30 +0200

    tidied up results and prepared final bits of model training

--- 2_finetuning/launch_ft2.sh
+++ 2_finetuning/launch_ft2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=ft2-xd20-hi19
+#SBATCH --job-name=ft2-xd0-hi21
 #SBATCH --clusters=arc
 #SBATCH --ntasks-per-node=16
 #SBATCH --time=11:59:00
@@ -20,9 +20,9 @@ module load Anaconda3/2020.11
 source activate $DATA/conda-envs/lrh-env
 
 # Pick base model to then continue finetuning (-->FT2)
-basemodel="xlmt_dyn21_en_20000_rs1"
+basemodel="xlmt_dyn21_en_0_rs1"
 
-for dataset in has19_hi has20_hi has21_hi; do
+for dataset in has21_hi; do
     for split in 10 20 30 40 50 100 200 300 400 500 1000 2000; do
         for seed in rs1 rs2 rs3 rs4 rs5 rs6 rs7 rs8 rs9 rs10; do
             python finetune_and_test.py \
@@ -31,7 +31,6 @@ for dataset in has19_hi has20_hi has21_hi; do
                 --validation_file $DATA/low-resource-hate/0_data/main/1_clean/${dataset}/dev_*.csv \
                 --dataset_cache_dir $DATA/low-resource-hate/z_cache/datasets \
                 --do_train \
-                --use_class_weights true \
                 --per_device_train_batch_size 16 \
                 --num_train_epochs 5 \
                 --max_seq_length 128 \
@@ -42,7 +41,7 @@ for dataset in has19_hi has20_hi has21_hi; do
                 --save_total_limit 1 \
                 --load_best_model_at_end \
                 --metric_for_best_model "macro_f1" \
-                --output_dir $DATA/low-resource-hate/finetuned-models/random-sample/multilingual-models/${basemodel}_w_${dataset}_${split}_${seed} \
+                --output_dir $DATA/low-resource-hate/finetuned-models/random-sample/multilingual-models/${basemodel}_${dataset}_${split}_${seed} \
                 --overwrite_output_dir
 
             rm -rf $DATA/low-resource-hate/finetuned-models/random-sample/multilingual-models/${basemodel}_w_${dataset}_${split}_${seed}/check*
